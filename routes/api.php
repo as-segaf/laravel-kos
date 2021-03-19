42582d95b5a23ae5a2407c9dd910cc352fd7f85d
as-segaf <ziyanassegaf20@gmail.com>
2021-03-19 09:16:33 +0700

    add new route for logout

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
