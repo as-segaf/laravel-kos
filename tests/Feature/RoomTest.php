2a3198e851a5f9b68df00528497c4e5912ebdb46
as-segaf <ziyanassegaf20@gmail.com>
2021-03-21 08:08:34 +0700

    add create room tests

--- tests/Feature/RoomTest.php
+++ tests/Feature/RoomTest.php
@@ -3,8 +3,11 @@
 namespace Tests\Feature;
 
 use App\Models\Room;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class RoomTest extends TestCase
@@ -42,6 +45,99 @@ class RoomTest extends TestCase
                         'status'
                     ]
                 ] 
+            ]);
+    }
+
+    public function testSuccessfulCreateRoom()
+    {
+        $this->withoutExceptionHandling();
+        $user = User::create([
+            'name' => 'testing',
+            'email' => 'testing@testing.com',
+            'password' => Hash::make('testing123'),
+            'is_admin' => 1
+        ]);
+
+        $room = [
+            'name' => 'room 1',
+            'description' => 'a good room',
+            'length' => 3,
+            'width' => 2,
+            'status' => 'unused'
+        ];
+
+        Sanctum::actingAs($user);
+
+        $this->json('post', '/api/room', $room, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'code',
+                'message',
+                'data' => [
+                    'id',
+                    'name',
+                    'description',
+                    'length',
+                    'width',
+                    'status'
+                ]
+            ]);
+            
+        $this->assertDatabaseCount('rooms', 1);
+        $this->assertDatabaseHas('rooms', $room);
+    }
+
+    public function testOnlyAdminCanCreateRoom()
+    {
+        $this->withoutExceptionHandling();
+        $user = User::create([
+            'name' => 'testing',
+            'email' => 'testing@testing.com',
+            'password' => Hash::make('testing123'),
+            'is_admin' => 0
+        ]);
+
+        $room = [
+            'name' => 'room 1',
+            'description' => 'a good room',
+            'length' => 3,
+            'width' => 2,
+            'status' => 'unused'
+        ];
+
+        Sanctum::actingAs($user);
+        
+        $this->json('post', '/api/room', $room, ['Accept' => 'application/json'])
+            ->assertStatus(403)
+            ->assertJson([
+                'code' => 403,
+                'message' => 'You are not allowed to do this action.'
+            ]);
+            
+        $this->assertDatabaseCount('rooms', 0);
+    }
+
+    public function testRequiredFieldsForCreateRoom()
+    {
+        $user = User::create([
+            'name' => 'testing',
+            'email' => 'testing@testing.com',
+            'password' => Hash::make('testing123'),
+            'is_admin' => 0
+        ]);
+
+        Sanctum::actingAs($user);
+
+        $this->json('post', '/api/room', ['Accept' => 'application/json'])
+            ->assertStatus(422)
+            ->assertJson([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'name' => ['The name field is required.'],
+                    'length' => ['The length field is required.'],
+                    'width' => ['The width field is required.'],
+                    'status' => ['The status field is required.']
+                ]
             ]);
     }
 }
