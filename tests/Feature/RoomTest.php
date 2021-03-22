4c6fa3283ba9946c494c57b0a09121aac7dbe39e
as-segaf <ziyanassegaf20@gmail.com>
2021-03-22 19:56:13 +0700

    add new tests for update & show room

--- tests/Feature/RoomTest.php
+++ tests/Feature/RoomTest.php
@@ -14,6 +14,37 @@ class RoomTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function createRoom()
+    {
+        return Room::create([
+            'name' => 'room 1',
+            'description' => 'a good room',
+            'length' => 2,
+            'width' => 1,
+            'status' => 'unused'
+        ]);
+    }
+
+    public function createAdmin()
+    {
+        return User::create([
+            'name' => 'testing',
+            'email' => 'testing@testing.com',
+            'password' => Hash::make('testing123'),
+            'is_admin' => 1
+        ]);
+    }
+
+    public function createUser()
+    {
+        return User::create([
+            'name' => 'testing',
+            'email' => 'testing@testing.com',
+            'password' => Hash::make('testing123'),
+            'is_admin' => 0
+        ]);
+    }
+
     public function testGuestCanSeeRoomList()
     {
         $this->withoutExceptionHandling();
@@ -51,12 +82,7 @@ class RoomTest extends TestCase
     public function testSuccessfulCreateRoom()
     {
         $this->withoutExceptionHandling();
-        $user = User::create([
-            'name' => 'testing',
-            'email' => 'testing@testing.com',
-            'password' => Hash::make('testing123'),
-            'is_admin' => 1
-        ]);
+        $user = $this->createAdmin();
 
         $room = [
             'name' => 'room 1',
@@ -90,12 +116,7 @@ class RoomTest extends TestCase
     public function testOnlyAdminCanCreateRoom()
     {
         $this->withoutExceptionHandling();
-        $user = User::create([
-            'name' => 'testing',
-            'email' => 'testing@testing.com',
-            'password' => Hash::make('testing123'),
-            'is_admin' => 0
-        ]);
+        $user = $this->createUser();
 
         $room = [
             'name' => 'room 1',
@@ -119,12 +140,7 @@ class RoomTest extends TestCase
 
     public function testRequiredFieldsForCreateRoom()
     {
-        $user = User::create([
-            'name' => 'testing',
-            'email' => 'testing@testing.com',
-            'password' => Hash::make('testing123'),
-            'is_admin' => 0
-        ]);
+        $user = $this->createAdmin();
 
         Sanctum::actingAs($user);
 
@@ -137,6 +153,164 @@ class RoomTest extends TestCase
                     'length' => ['The length field is required.'],
                     'width' => ['The width field is required.'],
                     'status' => ['The status field is required.']
+                ]
+            ]);
+    }
+
+    public function testSuccessfulUpdateRoom()
+    {
+        $this->withoutExceptionHandling();
+
+        $room = $this->createRoom();
+
+        $updateData = [
+            'name' => 'new room',
+            'description' => 'a very good room',
+            'length' => 5,
+            'width' => 4,
+            'status' => 'used'
+        ];
+
+        $user = $this->createAdmin();
+        
+        Sanctum::actingAs($user);
+
+        $this->json('patch', '/api/room/'.$room->id, $updateData, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'code',
+                'message',
+                'data' => [
+                    'id',
+                    'name',
+                    'description',
+                    'length',
+                    'width',
+                    'status'
+                ]
+            ]);
+        
+        $this->assertDatabaseMissing('rooms', [
+            'name' => 'room 1',
+            'description' => 'a good room',
+            'length' => 2,
+            'width' => 1,
+            'status' => 'unused'
+        ]);
+        $this->assertDatabaseHas('rooms', $updateData);
+    }
+
+    public function testOnlyAdminCanUpdateRoom()
+    {
+        $this->withoutExceptionHandling();
+
+        $room = $this->createRoom();
+
+        $updateData = [
+            'name' => 'new room',
+            'description' => 'a very good room',
+            'length' => 5,
+            'width' => 4,
+            'status' => 'used'
+        ];
+
+        $user = $this->createUser();
+        
+        Sanctum::actingAs($user);
+
+        $this->json('patch', '/api/room/'.$room->id, $updateData, ['Accept' => 'application/json'])
+            ->assertStatus(403)
+            ->assertJson([
+                'code' => 403,
+                'message' => 'You are not allowed to do this action.'
+            ]);
+        
+        $this->assertDatabaseHas('rooms', [
+            'name' => 'room 1',
+            'description' => 'a good room',
+            'length' => 2,
+            'width' => 1,
+            'status' => 'unused'
+        ]);
+    }
+
+    public function testRequiredFieldsForUpdateRoom()
+    {
+        $room = Room::create([
+            'name' => 'room 1',
+            'description' => 'a good room',
+            'length' => 2,
+            'width' => 1,
+            'status' => 'unused'
+        ]);
+
+        $user = User::create([
+            'name' => 'testing',
+            'email' => 'testing@testing.com',
+            'password' => Hash::make('testing123'),
+            'is_admin' => 1
+        ]);
+        
+        Sanctum::actingAs($user);
+
+        $this->json('patch', '/api/room/'.$room->id, ['Accept' => 'application/json'])
+            ->assertStatus(422)
+            ->assertJson([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'name' => ['The name field is required.'],
+                    'length' => ['The length field is required.'],
+                    'width' => ['The width field is required.'],
+                    'status' => ['The status field is required.']
+                ]
+            ]);
+    }
+
+    public function testUpdateWithWrongIdFailed()
+    {
+        $this->withoutExceptionHandling();
+
+        $room = $this->createRoom();
+        $admin = $this->createAdmin();
+        $updateData = [
+            'name' => 'new room',
+            'description' => 'a new updated room',
+            'length' => 5,
+            'width' => 4,
+            'status' => 'used'
+        ];
+
+        Sanctum::actingAs($admin);
+
+        $this->json('patch', '/api/room/123', $updateData, ['Accept' => 'application/json'])
+            ->assertStatus(404)
+            ->assertJsonStructure([
+                'code',
+                'message'
+            ]);
+    }
+
+    public function testGuestCanSeeARoom()
+    {
+        $this->withoutExceptionHandling();
+
+        $room = $this->createRoom();
+        $user = $this->createUser();
+        
+        Sanctum::actingAs($user);
+
+        $this->json('get', '/api/room/'.$room->id, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'code',
+                'message',
+                'data' => [
+                    'id',
+                    'name',
+                    'description',
+                    'length',
+                    'width',
+                    'status'
                 ]
             ]);
     }
