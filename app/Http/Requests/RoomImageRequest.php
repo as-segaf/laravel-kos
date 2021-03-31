1bd68f915c6f27ded9e2b745f35b9f7d2d05b618
as-segaf <ziyanassegaf20@gmail.com>
2021-03-31 11:29:10 +0700

    create store & update room image validation

---
+++ app/Http/Requests/RoomImageRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RoomImageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        if ($this->method() == 'POST') {
+            return [
+                'room_id' => 'required',
+                'img_name' => 'required'
+            ];
+        }
+
+        if ($this->method() == 'PATCH' || $this->method() == 'PUT') {
+            return [
+                'img_name' => 'required'
+            ];
+        }
+    }
+}
