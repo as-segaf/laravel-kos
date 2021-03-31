4d742b69e570c76d7982d7ff283da833bd746e12
as-segaf <ziyanassegaf20@gmail.com>
2021-03-31 09:20:20 +0700

    add room image route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\RoomController;
 use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\RoomImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -28,7 +29,8 @@ Route::get('/room', [RoomController::class, 'index']);
 Route::get('/room/{room}', [RoomController::class, 'show']);
 
 Route::middleware('auth:sanctum')->group(function() {
-    Route::resource('room', RoomController::class)->only('store', 'update', 'destroy');
     Route::resource('order', OrderController::class)->except('create', 'edit');
+    Route::resource('room', RoomController::class)->only('store', 'update', 'destroy');
+    Route::resource('roomImage', RoomImageController::class);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
