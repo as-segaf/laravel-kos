d14e9f11ecc94eb79edc8b43512e7a92ca6bdfae
as-segaf <ziyanassegaf20@gmail.com>
2021-03-31 10:00:26 +0700

    bind room image interface & repository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -3,9 +3,11 @@
 namespace App\Providers;
 
 use App\Interfaces\OrderRepositoryInterface;
+use App\Interfaces\RoomImageRepositoryInterface;
 use App\Interfaces\RoomRepositoryInterface;
 use App\Interfaces\UserInterface;
 use App\Repositories\OrderRepository;
+use App\Repositories\RoomImageRepository;
 use App\Repositories\RoomRepository;
 use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
@@ -22,6 +24,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(UserInterface::class, UserRepository::class);
         $this->app->bind(RoomRepositoryInterface::class, RoomRepository::class);
         $this->app->bind(OrderRepositoryInterface::class, OrderRepository::class);
+        $this->app->bind(RoomImageRepositoryInterface::class, RoomImageRepository::class);
     }
 
     /**
