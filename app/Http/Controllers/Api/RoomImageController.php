0c6a164ce0edb45347411636a8ace0a6321a55cf
as-segaf <ziyanassegaf20@gmail.com>
2021-03-31 09:20:07 +0700

    create room image service & controller

---
+++ app/Http/Controllers/Api/RoomImageController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Services\RoomImageService;
+use App\Traits\ResponseStructure;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\Request;
+
+class RoomImageController extends Controller
+{
+    use ResponseStructure;
+
+    protected $roomImageService;
+
+    public function __construct(RoomImageService $roomImageService)
+    {
+        return $this->roomImageService = $roomImageService;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $data = $this->roomImageService->store($request);
+        } catch (AuthorizationException $exception) {
+            return $this->errorResponse(403, $exception->getMessage());
+        } catch (\Exception $exception) {
+            return $this->errorResponse(500, $exception->getMessage());
+        }
+
+        return $this->successResponse(200, 'success', $data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Services/RoomImageService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+use App\Interfaces\RoomImageRepositoryInterface;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Support\Facades\Gate;
+
+class RoomImageService
+{
+    protected $roomImageRepository;
+
+    public function __construct(RoomImageRepositoryInterface $roomImageRepository)
+    {
+        return $this->roomImageRepository = $roomImageRepository;
+    }
+
+    public function store($request)
+    {
+        if (Gate::allows('isAdmin')) {
+            return $this->roomImageRepository->createRoomImage($request);
+        }
+
+        throw new AuthorizationException('You are not allowed to do this action.');
+    }
+}
